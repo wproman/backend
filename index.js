907562c599b3f7efe07d1c23b23010f8fa9566ef
wproman <rmn.tda@gmail.com>
2024-12-20 22:28:57 +0600

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const productRouter = require("./routes/product");
 connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
+// Enable CORS
+app.use(cors());
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
