4882c0ea0d65b3af2c67995fbdb3b3e6fa1af574
wproman <rmn.tda@gmail.com>
2024-12-20 12:36:54 +0600

    Introducing Router from express. Replace app.get().post() and adding router.get()

--- index.js
+++ index.js
@@ -1,71 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model");
+const productController = require("./controller/product");
+const productRouter = express.Router();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/r", productRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    console.log(req.body);
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    res.status(200).json({ message: "Product deleted" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+productRouter
+  .post("/api/products", productController.create)
+  .get("/api/products", productController.getAll)
+  .get("/api/product/:id", productController.getOne)
+  .patch("/api/product/:id", productController.update)
+  .delete("/api/product/:id", productController.deleteOne);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
